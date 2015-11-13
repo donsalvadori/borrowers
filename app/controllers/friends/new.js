cc74ebc331effbb06eee8d83de41b19f82ace67f
Chris Salvi <csalvi42@gmail.com>
2015-11-12 21:03:55 -0800

    pg 33 of tutorial

---
+++ app/controllers/friends/new.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	isValid: Ember.computed(
+		'model.email',
+		'model.firstName',
+		'model.lastName',
+		'model.twitter',
+		{
+			get() {
+				return !Ember.isEmpty(this.get('model.email')) &&
+					!Ember.isEmpty(this.get('model.firstName')) &&
+					!Ember.isEmpty(this.get('model.lastName')) &&
+					!Ember.isEmpty(this.get('model.twitter'));
+				}
+			}
+		),
+
+
+
+	actions: {
+		save() {
+			console.log('+- save action in friends new controller');
+
+			return true;
+			},
+		
+		cancel() {
+			console.log('+- cancel action in friends new controller');
+
+			return true;
+			}
+		}
+});
