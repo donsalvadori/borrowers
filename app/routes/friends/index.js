804a0875e0c312b13bbed980241ae14beea43803
Chris Salvi <csalvi42@gmail.com>
2015-11-15 11:34:25 -0800

    pg 56

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  queryParams: {
-    sortBy: {
-      refreshModel: true
-    },
-    sortAscending: {
-      refreshModel: true
-    }
-  },
-  model(params) {
-    return this.store.query('friend', params);
-  }
+	model() {
+		return this.store.findAll('friend');
+	}
 });
